efd585220ee8a223faef0451b8b0ee78488de4a0
Andrew Smith <smitha24@rpi.edu>
2021-02-14 00:32:42 -0600

    Remove debug code

--- server.py
+++ server.py
@@ -25,23 +25,16 @@ class Server:
         print(f"Accepted Connection from {addr}")
         while True:
             try:
-                # command_length = conn.recv(4096).decode('utf-8')
-                # print(repr(command_length))
-                # command_length = int(command_length.strip())
-                # assert command_length > 0
-                # conn.sendall(b"Ack\r\n")
-                # output = conn.recv(command_length+2).decode('utf-8')
-                # print(repr(output))
                 counter = 0
                 message = ""
                 while counter < 1:
-                    partial_message = conn.recv(2).decode('utf-8')
+                    partial_message = conn.recv(256).decode('utf-8')
                     for c in partial_message:
                         message += c
                         if c == '\n':
                             counter += 1
                 print(message)
-                # self.parse_command(output)
+                self.parse_command(message)
             except ConnectionResetError:
                 self.client.close()
                 break
